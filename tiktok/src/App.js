fe97a9aad7f59d214c01c0387a68579398fcaa59
him123456789 <sabhimanyu809@gmail.com>
2021-07-25 11:01:45 +0530

    Restraunt project

---
+++ tiktok/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import UseEffect from "./component/Hooks/useEffect";
+import Restraunt from "./component/Basics/Restraunt";
+// import UseState from "./component/Hooks/useState";
+//import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <Restraunt />;
+};
+
+export default App;
